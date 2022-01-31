7d71de16bae97c4ebba6b4ab87a6d130947ff770
JustKike <jaircervantes11@gmail.com>
2022-01-30 16:35:30 -0800

    Correccion y comentarios

--- crud_provider/addnew.php
+++ crud_provider/addnew.php
@@ -1,13 +1,14 @@
 <?php
+	// Conexcion con la BD
 	include('conn.php');
-
+	// Guardarmos los datos de los input en variables
 	$nombre=$_POST['nombre'];
 	$direccion=$_POST['direccion'];
 	$telefono=$_POST['telefono'];
 	$correo=$_POST['correo'];
-
+	// Agregamos el query para insertar los datos dentro de la tabla en la BD
 	mysqli_query($conn,"insert into proveedor (nombre,direccion,telefono,correo)
-	values ('$nombre','$direccion','$correo', '$telefono')");
+	values ('$nombre','$direccion','$telefono','$correo')");
+	// Redirigimos al index
 	header('location:index.php');
-
 ?>
